b96aecb96e7b2f5182b7b0f411288a336e7a21e2
TamiresLucena <tamiresb.lucena@gmail.com>
2022-10-20 20:17:11 -0300

    chore: add knex migration

---
+++ backend/src/database/migrations/20221020143054_products.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('products', function (table) {
+    table.increments('id');
+    table.string('name').notNullable();
+    table.string('price').notNullable();
+    table.string('brand').notNullable();
+    table.string('image').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('products');
+};
